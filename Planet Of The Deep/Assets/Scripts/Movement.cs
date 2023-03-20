8e60073989714c23bfef09abfbc7bf128adda2cb
Youssef El-Guoshi <elguoshis@gmail.com>
2023-03-20 15:15:40 +0000

    Fixed movement script

--- Planet Of The Deep/Assets/Scripts/Movement.cs
+++ Planet Of The Deep/Assets/Scripts/Movement.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 
-public class Movement: MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float jumpForce = 10f;
+    public float moveSpeed = 5f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     public bool isGrounded;
+
 
     private void Start()
     {
@@ -20,9 +22,22 @@ public class Movement: MonoBehaviour
         // Check if the player is on the ground
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (moveInput < 0 )
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (moveInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
     }
+
 }
