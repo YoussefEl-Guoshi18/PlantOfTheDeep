feac4b92fca7157a301016a67c6009b55b18f950
YoussefEl-Guoshi18 <elguoshis@gmail.com>
2023-05-01 09:12:03 +0100

    Added door boss script reworked boss tiles

---
+++ Planet Of The Deep/Assets/Scripts/DoorOpenScript/DoorOpenBoss.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorOpenBoss : MonoBehaviour
+{
+    bool enterNextScene = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enterNextScene = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enterNextScene == true && Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Boss");
+        }
+    }
+
+
+}
